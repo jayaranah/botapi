f098517d0b8a259db282e8b7b8d7d30857e7c0a0
ikraduya <ikraduya@gmail.com>
2017-07-12 20:46:08 +0700

    add ImageSendMessage import

--- app.py
+++ app.py
@@ -104,8 +104,8 @@ def handle_text_message(event):
         elif text == '#bye':
             if isinstance(event.source, SourceGroup) or isinstance(event.source, SourceRoom):
                 image_message = ImageSendMessage(
-                    original_content_url='https://s-media-cache-ak0.pinimg.com/736x/8e/0e/f1/8e0ef15e0ba7a598dbe69658c7b38379--nerd-art-cosplay-anime.jpg',
-                    preview_image_url='https://a.wattpad.com/useravatar/tori_tatsu.128.745644.jpg')
+                    original_content_url='https://s-media-cache-ak0.pinimg.com/736x/8e/0e/f1/8e0ef15e0ba7a598dbe69658c7b38379--nerd-art-cosplay-anime.jpg'
+                    )
                 line_bot_api.reply_message(event.reply_token, image_message)
                 try:
                     line_bot_api.leave_group(event.source.group_id)
