9324de390076640dc660720ed0f0098b31044405
ikraduya <ikraduya@gmail.com>
2017-07-13 19:17:21 +0700

    update info cmd

--- app.py
+++ app.py
@@ -111,8 +111,12 @@ def handle_text_message(event):
             plus = ''
             if eh == '':
                 plus = ' sayang'
-            line_bot_api.reply_message(
-                    event.reply_token, TextSendMessage(text=eh + profile.display_name+ plus +',\n'+choice(list_gombal)))
+            try:
+                line_bot_api.reply_message(
+                        event.reply_token, TextSendMessage(text=eh + profile.display_name+ plus +',\n'+choice(list_gombal)))
+            except:
+                line_bot_api.reply_message(
+                    event.reply_token, TextSendMessage(text="Ga ada gombal buat mu..")
         # help
         elif cmd.group(1) == 'help':
             if cmd.group() == '#help':
