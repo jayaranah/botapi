2e7e4390e2b664ea239bf610f0f4ed79e3d07827
ikraduya <ikraduya@gmail.com>
2017-07-13 15:04:20 +0700

    pisahkan var from main app

--- app.py
+++ app.py
@@ -83,17 +83,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     text = event.message.text
-    daftar_jurus = {'naga kacang':'nagakacang.txt'}
-    daftar_cmd = ['bye', 'help', 'info', 'jurus', 'tag', 'tags', 'ougi', 'panggil', 'profil']
-    daftar_tag = ['gagal ambis']
-    img_url = {'bye':
-                   ['https://image.ibb.co/ibvkKa/akatsukileave.jpg','https://image.ibb.co/meYGQF/akatsukileave_prev.jpg'],
-               'gagal ambis':
-                   ['https://image.ibb.co/hH2Msv/gagal_ngambis.jpg','https://image.ibb.co/mkb35F/gagal_ngambis_prev.jpg'],
-            
-                }
+    
     if text[0] == '#':
         cmd = search(r'\#(\w*)\s*(.*)', text)
+        from var import *
         # bye
         if cmd.group() == '#bye':
             if isinstance(event.source, SourceGroup) or isinstance(event.source, SourceRoom):
